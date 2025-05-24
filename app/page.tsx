d67e2adde8f1adb6457adf4a447a54bab95e170e
Nisarg Trivedi <69729785+NT1906@users.noreply.github.com>
2025-05-24 11:32:24 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import { socialLinks } from "./config";
 export default function Page() {
   return (
     <section>
-      <a href="/">
+      <a href="/IMG_0827.HEIC">
         <Image
-          src="/profile.jpeg"
+          src="/.jpeg"
           alt="Profile photo"
           className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
           unoptimized
