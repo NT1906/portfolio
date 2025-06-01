009cff7ee71b955482300be3ce707b2d356d99da
Nisarg Trivedi <69729785+NT1906@users.noreply.github.com>
2025-06-01 12:45:18 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
           className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
           unoptimized
           width={160}
-          height={160}
+          height={140}
           priority
         />
       </a>
